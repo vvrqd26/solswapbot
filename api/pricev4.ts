638eb42180036f848da010a6a89f067d9a06e6f6
luya <1456887752@qq.com>
2023-11-26 21:44:47 +0800

    update

--- api/pricev4.ts
+++ api/pricev4.ts
@@ -6,6 +6,7 @@ interface IPriceInfo {
     vsTokenSymbol: string,
     price: number
 }
+// 获取价格
 export const getPrice = async (token: string, vsToken: string = 'So11111111111111111111111111111111111111112') => {
     const res = await fetch(`${baseURL}price?ids=${token}&vsToken=${vsToken}`)
     const {data} = await res.json()
--- api/swapv6.ts
+++ api/swapv6.ts
@@ -1,17 +1,17 @@
 import { getKeypair } from "../models/account.ts";
-import {} from 'npm:@solana/spl-token'
 import {VersionedTransaction} from 'npm:@solana/web3.js';
 import * as base64 from "https://denopkg.com/chiefbiiko/base64/mod.ts";
 import { connection } from "./token.ts";
 
 const baseURL = "https://quote-api.jup.ag/v6/"
 
+// 获取交易对路由
 export const getQuote = async (inputMint:string,outputMint:string,amount:number,slippageBps:number) => {
     const res = await fetch(`${baseURL}quote?inputMint=${inputMint}&outputMint=${outputMint}&amount=${amount}&slippageBps=${slippageBps}`)
     const data = await res.json()
     return data
 }
-
+// 获取swap transaction
 export const getSwapTx = async (id:number,inputMint:string,outputMint:string,amount:number,slippageBps:number) => {
     const keyPair = await getKeypair(id)
     const quoteResponse = await getQuote(inputMint,outputMint,amount,slippageBps)
@@ -46,7 +46,7 @@ export const getSwapTx = async (id:number,inputMint:string,outputMint:string,amo
       transaction.sign([keyPair]);  
       return transaction.serialize()    
 }
-
+// 发送交易
 export const execTx = async (tx:Uint8Array)=>{
     const txid =  await connection.sendRawTransaction(tx,{
         skipPreflight: true,
